2e9069bc9fbb50c76d1a11ea9ac5151b76730c05
James Johnson <jtaylorjohnson@gmail.com>
2020-04-09 19:38:12 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,14 @@
 class Backer
   attr_accessor :name, :backed_projects
-  @projects = []
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   
   def back_project(project)
-    @projects << project
+    @backed_projects << project
   end   
-  
-  def backed_projects
-    @projects
-  end
+
   
 end
